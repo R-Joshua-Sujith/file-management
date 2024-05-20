53762c19d6386698f2c0706ff97d6edfe354e770
R-Joshua-Sujith <joshuasujith14@gmail.com>
2024-05-20 12:58:53 +0530

    completed

--- routes/user.js
+++ routes/user.js
@@ -180,6 +180,25 @@ router.put('/directory/:directoryId/permissions/remove', adminVerify, async (req
     }
 });
 
+router.put('/update-user/:id', adminVerify, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { password } = req.body;
+        const user = await User.findById(id)
+
+        if (!user) {
+            return res.status(404).json({ error: 'User Not Found' })
+        }
+
+        user.password = password
+        await user.save();
+        res.status(200).json({ message: "User Password Updated Successfully" })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Server Error" })
+    }
+})
+
 
 
 module.exports = router;
